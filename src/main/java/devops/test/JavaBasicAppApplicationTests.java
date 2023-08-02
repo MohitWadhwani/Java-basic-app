72fe0dd38ffa26461c34eab8d21232cd2c643263
Mohit <greatwadhwani@gmail.com>
2023-08-03 01:44:36 +0530

    Add Code

--- src/main/java/devops/test/JavaBasicAppApplicationTests.java
+++ src/main/java/devops/test/JavaBasicAppApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.Javabasicapp;
 
+
+/*
+ * Developed By Mohit Wadhwani
+ */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaBasicAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JavaBasicAppApplication.class, args);
-	}
-
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			SpringApplication.run(JavaBasicAppApplication.class,args);
+		}
 }
